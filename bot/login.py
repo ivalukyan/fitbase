a1b07161529128914934944b2e079379fc5f9173
ivanlukanov <ivalukyanov1@gmail.com>
2024-12-28 01:59:17 +0300

    Upd: login.py

--- bot/login.py
+++ bot/login.py
@@ -1,3 +1,4 @@
+import asyncio
 from uuid import uuid4
 from aiogram import Router, F
 from aiogram.types import (
@@ -5,6 +6,7 @@ from aiogram.types import (
 )
 
 from menu import menu
+from utils import get_all_contacts
 
 
 router = Router()
@@ -24,7 +26,9 @@ async def login(call: CallbackQuery) -> None:
 async def process_contact(message: Message) -> None:
     user_phone = message.contact.phone_number
 
-    if user_phone == "79687518203":
+    contacts = await get_all_contacts()
+
+    if user_phone in contacts:
         await message.answer("Авторизация успешна! Добро пожаловать.")
         callback_query = CallbackQuery(id=str(uuid4()), from_user=message.from_user, data="menu",
                                        message=message, chat_instance="auth")
