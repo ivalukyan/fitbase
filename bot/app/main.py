8d70e68cd5888736c440b7e42385845b6475754c
ivanlukanov <ivalukyanov1@gmail.com>
2024-12-31 15:36:00 +0300

    Upd: errors

--- bot/app/main.py
+++ bot/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
 from admin_app.routers.admin import router as admin_router
 from admin_app.routers.auth import router as auth_router
 
@@ -9,6 +11,25 @@ api_router = APIRouter(
     prefix="/api",
     tags=["api"],
 )
+
+templates = Jinja2Templates("admin_app/templates")
+
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, exp: HTTPException):
+    return templates.TemplateResponse("errors.html", {"request": request, 'status': exp.status_code,
+                                                      'details': exp.detail})
+
+
+@app.exception_handler(500)
+async def custom_500_handler(request: Request, exp: HTTPException):
+    return templates.TemplateResponse("errors.html", {"request": request, 'status': exp.status_code,
+                                                      'details': exp.detail})
+
+
+@app.exception_handler(400)
+async def custom_400_handler(request: Request, exp: HTTPException):
+    return templates.TemplateResponse("errors.html", {"request": request, 'status': exp.status_code,
+                                                      'details': exp.detail})
 
 api_router.include_router(auth_router)
 api_router.include_router(admin_router)
