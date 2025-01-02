87361c9c32c31b479868ffd5ba114f99ac0c8e3e
ivanlukanov <ivalukyanov1@gmail.com>
2025-01-02 13:49:26 +0300

    Upd: backend for users

--- bot/app/admin_app/routers/admin.py
+++ bot/app/admin_app/routers/admin.py
@@ -29,9 +29,24 @@ async def get_admin_me(admin: AdminSchemas = Depends(get_current_admin)):
     return admin
 
 
-@router.get("/users")
+@router.get("/users", description="Получение всех пользователей")
 async def get_admin_users(request: Request, db: Session = Depends(get_db_session)):
     return templates.TemplateResponse("users.html", {"request": request})
+
+
+@router.post("users", description="Добавление пользователя")
+async def create_admin_user(db: Session = Depends(get_db_session)):
+    return {'msg': 'Добавление пользователя'}
+
+
+@router.put("/users/{id}", description='Обновление данных пользователя')
+async def update_admin_user(id: UUID, db: Session = Depends(get_db_session)):
+    return {'msg': 'Обновление пользователя'}
+
+
+@router.delete("/users/{id}", description='Удаление данных пользователя')
+async def delete_admin_user(id: UUID, db: Session = Depends(get_db_session)):
+    return {'msg', 'Удаление данных пользователя'}
 
 
 @router.get("/normative", description="Получение результатов пользователей")
