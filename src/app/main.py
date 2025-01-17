215aa775075432279e224aee6d13cbb3588a0089
Ivan <146388672+ivalukyan@users.noreply.github.com>
2025-01-18 01:42:42 +0300

    Dockerfile

--- src/app/main.py
+++ src/app/main.py
@@ -78,4 +78,4 @@ app.include_router(api_router)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
