f3dcb90119896bc1a0833f7fd88ff58a0ecb1c83
Ivan <146388672+ivalukyan@users.noreply.github.com>
2025-02-05 23:29:22 +0300

    fix

--- src/service/app_service.py
+++ src/service/app_service.py
@@ -1,18 +1,25 @@
 import os
 import sys
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from redis_db.main import redis
 
 
-async def get_count_month_users(mon: int, cnt: int) -> None:
+async def get_count_month_users(mon: int, cnt: int) -> list:
     key = mon - 1
-    value = await redis.get("list_users")
-    print(value)
-    if int(value[key]) < cnt:
-        value[key] = cnt
-        await redis.set("list_users", value)
 
-    print(await redis.get("list_users"))
-    return await redis.get("list_users")
+    value_bytes = await redis.get("list_users")
+    value_str = value_bytes.decode('utf-8')
+    value = json.loads(value_str)
+
+    if value[key] < cnt:
+        value[key] = cnt
+        await redis.set("list_users", json.dumps(value))
+
+    updated_value_bytes = await redis.get("list_users")
+    updated_value_str = updated_value_bytes.decode('utf-8')
+    updated_value = json.loads(updated_value_str)
+
+    return updated_value
